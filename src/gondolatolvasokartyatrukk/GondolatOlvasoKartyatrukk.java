8c49720ce5ed56f19b974c4cbdcf818630ded44a
Péter Csolti <csolti@szamalk-szalezi.hu>
2023-10-19 17:40:40 +0200

    fobb metodusok

---
+++ src/gondolatolvasokartyatrukk/GondolatOlvasoKartyatrukk.java
@@ -0,0 +1,32 @@
+package gondolatolvasokartyatrukk;
+
+public class GondolatOlvasoKartyatrukk {
+
+    static String[] pakli = new String[22];
+    
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            kirak();
+            melyik();
+            kever();
+        }
+        ezVolt();
+    }
+
+    private static void kirak() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static void melyik() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static void kever() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static void ezVolt() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
