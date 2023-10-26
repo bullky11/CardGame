925a25304f7aac4944059b3d4434ea5c1beb73f8
PatrisaRose <cseresznyevirag4@gmail.com>
2023-10-26 19:58:46 +0200

    case

--- src/gondolatolvasokartyatrukk/GondolatOlvasoKartyatrukk.java
+++ src/gondolatolvasokartyatrukk/GondolatOlvasoKartyatrukk.java
@@ -4,15 +4,16 @@ import java.util.Scanner;
 
 public class GondolatOlvasoKartyatrukk {
 ///
+
     static Scanner sc = new Scanner(System.in);
     static final int MERET = 22;
     static String[] pakli = new String[MERET];
-    
+
     public static void main(String[] args) {
         feltolt();
         for (int i = 0; i < 3; i++) {
             kirak();
-            melyik();     
+            melyik();
             kever(melyik());
         }
         ezVolt();
@@ -23,17 +24,17 @@ public class GondolatOlvasoKartyatrukk {
         String[] ertekek = {"Ász", "Kir", "Fel", "X", "IX", "VIII"};
 
         int db = 1;
-        for (int sz = 0;sz < szinek.length; sz++) {
-            for (int e = 0;db < MERET && e < ertekek.length; e++) {
+        for (int sz = 0; sz < szinek.length; sz++) {
+            for (int e = 0; db < MERET && e < ertekek.length; e++) {
                 pakli[db++] = szinek[sz] + "_" + ertekek[e];
             }
         }
     }
-    
+
     private static void kirak() {
         for (int i = 1; i < pakli.length; i++) {
             System.out.printf("%-7s", pakli[i]);
-            if(i % 3 == 0){
+            if (i % 3 == 0) {
                 System.out.println("");
             }
         }
@@ -42,32 +43,46 @@ public class GondolatOlvasoKartyatrukk {
     private static int melyik() {
         boolean jo;
         int oszlop;
-        do{
+        do {
             System.out.print("melyik oszlop (1-3): ");
-             oszlop = Integer.parseInt(sc.nextLine());
+            oszlop = Integer.parseInt(sc.nextLine());
             jo = oszlop >= 1 && oszlop <= 3;
-            
-        }while(!jo);
-       return oszlop;
+
+        } while (!jo);
+        return oszlop;
     }
 
     private static void kever(int oszlop) {
-        String[] uj = new String[MERET];
-        switch(oszlop){
-            case (1):;
-            case (2):;
-            case (3):for (int i = 1; i < 7; i++) {
-                uj[i] =pakli[21-(i-3)*3];
-                uj[i+7]=pakli[19-(i-1)*3];
-                uj[i+14]=pakli[20-(i-1)*3];
-                    
+        String[] ujPakli = new String[MERET];
+        switch (oszlop) {
+            case (1):
+                for (int i = 1; i <= 7; i++) {
+                    ujPakli[i] = pakli[20 - (i - 1) * 3];
+                    ujPakli[i + 7] = pakli[19 - (i - 1) * 3];
+                    ujPakli[i + 14] = pakli[21 - (i - 1) * 3];
+
                 }
-;
+                ;
+            case (2):
+                for (int i = 1; i < 7; i++) {
+                    ujPakli[i] = pakli[19 - (i - 1) * 3];
+                    ujPakli[i + 7] = pakli[20 - (i - 1) * 3];
+                    ujPakli[i + 14] = pakli[21 - (i - 1) * 3];
+                }
+                ;
+            case (3):
+                for (int i = 1; i < 7; i++) {
+                    ujPakli[i] = pakli[19 - (i - 1) * 3];
+                    ujPakli[i + 7] = pakli[20 - (i - 1) * 3];
+                    ujPakli[i + 14] = pakli[21 - (i - 1) * 3];
+                }
+                break;
         }
+        pakli = ujPakli;
     }
 
     private static void ezVolt() {
         System.out.println("A gondolt lap: " + pakli[11]);
     }
-    
+
 }
